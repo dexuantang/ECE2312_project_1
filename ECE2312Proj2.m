4988d70baa283fc5e56bbfa2e2b5d181dd57d599
DexuanTang <89551938+DexuanTang@users.noreply.github.com>
2023-02-15 19:46:55 -0500

    add a few functions

--- ECE2312Proj2.m
+++ ECE2312Proj2.m
@@ -12,20 +12,29 @@ FF = 8000;
 %% Select audio devices
 [user_selected_input_ID, user_slected_output_ID] = get_user_selected_device();
 
+%% generate, save and plot the 5000Hz sine
 sine = sinewave(record_length, 5000, FS);
 save_audio_to_wav(sine, FS);
-[sine_loaded,sine_FS_loaded] = load_audio_from_wav();
-plot_spectrogram(sine_loaded, sine_FS_loaded, "Spectrogram of 5000Hz sine");
+plot_spectrogram(sine, FS, "Spectrogram of 5000Hz sine");
 
+%% generate and plot 0 to 8000Hz chirp
 chirp = sine_chirp(record_length, FI, FF, FS);
 plot_spectrogram(chirp, FS, "Spectrogram of chirp");
+
+%% generate, save, and plot cetk sounds
 cetk_sound = cetk(record_length, FS);
+save_audio_to_wav(cetk_sound, FS);
 plot_spectrogram(cetk_sound, FS, "Spectrogram of cetk");
 
 
 %% load fox and plot
 [audio_data_loaded_1,FS_loaded] = load_audio_from_wav();
-plot_spectrogram(audio_data_loaded_1, FS_loaded, "Spectrogram of the first WAV");
+plot_spectrogram(audio_data_loaded_1, FS_loaded, "Spectrogram of brown fox");
+
+%% add sine wave to loaded data, save file, and plot
+sine_added = add_sine(audio_data_loaded_1, sine');
+save_audio_to_wav(sine_added, FS);
+plot_spectrogram(sine_added, FS_loaded, "Spectrogram of sine added to brown fox");
 
 %% A function that lets the user select their audio device by typing them in the command window
 function [input_device_ID, output_device_ID] = get_user_selected_device()
@@ -40,6 +49,7 @@ function [input_device_ID, output_device_ID] = get_user_selected_device()
     output_device_ID = input ('please select output device by typing its ID'); %get user input
 end
 
+%% function that goes chirp chirp
 function [chirp] = sine_chirp(record_length, FI, FF, FS)
     chirp = zeros;
     n = 0:(1/FS):(record_length);
@@ -48,26 +58,50 @@ function [chirp] = sine_chirp(record_length, FI, FF, FS)
         chirp(i) = sin(pi*f(i)*n(i));
     end
 end
+
+%% function that makes constant frequence sine
 function [output] = sinewave(record_length, F, FS)
     n = 0:(1/FS):(record_length);
     output = sin(2*pi*n*F);
 end
 
-%% CETK notes frequency: 1174.66, 1318.51, 1046.50, 523.25, 783.99
+%% function that makes alien sounds
 function [cetk] = cetk(record_length, FS)
+    %% initiate variables
     cetk = zeros;
-    note_durations = [0.5 0.7 1 0.7 4];
+    note_durations = [0.5 0.7 1 0.7 3];
     n = 0:(1/FS):(record_length);
     f = zeros;
-    f(1:(FS*note_durations(1))) = 1174.66;
-    f((1+FS*note_durations(1)):(FS*note_durations(2))) = 1318.51;
-    f((1+FS*note_durations(2)):(FS*note_durations(3))) = 1046.5;
-    f((1+FS*note_durations(3)):(FS*note_durations(4))) = 523.25;
-    f((1+FS*note_durations(4)):(FS*note_durations(5))) = 783.99;
+    %% note defination(D6, E6, C6, C5, G5)
+    f(1:(FS*note_durations(1))) = 2349.32;
+    f((1+FS*note_durations(1)):...
+        (FS*note_durations(2)+ FS*note_durations(1))) = 2637.02;
+    f((1+FS*(note_durations(1)+note_durations(2))):...
+        FS*(note_durations(1)+note_durations(2)+note_durations(3))) = 2093;
+    f((1+FS*(note_durations(1)+note_durations(2)+note_durations(3))):...
+        FS*(note_durations(1)+note_durations(2)+...
+        note_durations(3)+note_durations(4))) = 1046.5;
+    f((1+FS*(note_durations(1)+note_durations(2)+note_durations(3)+note_durations(4))):...
+        FS*(note_durations(1)+note_durations(2)+...
+        note_durations(3)+note_durations(4)+note_durations(5))) = 1567.98;
+    %% Variable frequency sine
     for i = 1:length(f)
         cetk(i) = sin(pi*f(i)*n(i));
     end
 end
+
+%% function that adds two signals together elementwise
+function [sine_added] = add_sine(voice, sine)
+    %% chop the signal incase they are different length
+    if length(voice) > length(sine)
+        voice = voice(1:length(sine));
+    elseif length(voice) < length(sine)
+        sine = sine(1:length(voice));
+    end
+    voice = voice(1:length(sine));
+    sine_added = voice + sine;
+end
+
 
 %% A function that creates a recorder objects and records audio with specified parameters
 function [myVoice, audio_data] = record_voice(FS, nBits, nChannels, length, ID)
